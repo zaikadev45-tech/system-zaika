4226724360af8fdadebb5552552a5f812c3f5812
zaikadev45-tech <252950044+zaikadev45-tech@users.noreply.github.com>
2026-03-21 19:02:27 -0300

    Novos audios + otimização no sistema

---
+++ system.sh
@@ -0,0 +1,12 @@
+#VIDEO
+debug.composition.type=gpu
+persist.sys.composition.type=gpu
+persist.sys.use_dithering=0
+
+#CPU
+CPU_MIN_CHECK_DURATION=false
+MIN_CRASH_INTERVAL=false
+SERVICE_TIMEOUT=false
+
+#LOG
+ro.logd.size.stats=false
